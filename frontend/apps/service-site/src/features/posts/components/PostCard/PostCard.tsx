931f658c7334ca5389a92dc9938124c6037926ac
yamatsum <sy12250@gmail.com>
2024-10-19 22:27:19 +0900

    feat: add post card

---
+++ frontend/apps/service-site/src/features/posts/components/PostCard/PostCard.tsx
@@ -0,0 +1,38 @@
+import type { Lang } from '@/features/i18n'
+import type { Post } from 'contentlayer/generated'
+import Image from 'next/image'
+import Link from 'next/link'
+import type { FC } from 'react'
+import { createPostDetailLink } from '../../utils'
+import styles from './PostCard.module.css'
+
+type Props = {
+  post: Post
+  lang?: Lang
+}
+
+export const PostCard: FC<Props> = ({ post, lang }) => {
+  return (
+    <div className={styles.postCard}>
+      <Link href={createPostDetailLink({ lang, slug: post.slug })}>
+        <div className={styles.inner}>
+          <Image
+            src={post.image}
+            alt={`Image of ${post.title}`}
+            width={328}
+            height={246}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+          />
+          <div className={styles.footer}>
+            <h2 className={styles.title}>{post.title}</h2>
+            <p>Text by {post.writer}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
--- frontend/apps/service-site/src/features/posts/components/PostListPage/PostListPage.tsx
+++ frontend/apps/service-site/src/features/posts/components/PostListPage/PostListPage.tsx
@@ -1,26 +1,8 @@
 import { type Lang, fallbackLang, getTranslation } from '@/features/i18n'
-import { MDXContent } from '@/libs/contentlayer'
+import { PostCard } from '@/features/posts'
 import type { Post } from 'contentlayer/generated'
-import { format, parseISO } from 'date-fns'
-import Link from 'next/link'
 import type { FC } from 'react'
-import { createPostDetailLink } from '../../utils'
-
-function PostCard(post: Post, lang?: Lang) {
-  return (
-    <div>
-      <h2>
-        <Link href={createPostDetailLink({ lang, slug: post.slug })}>
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.publishedAt}>
-        {format(parseISO(post.publishedAt), 'LLLL d, yyyy')}
-      </time>
-      <MDXContent code={post.body.code} />
-    </div>
-  )
-}
+import styles from './PostListPage.module.css'
 
 type Props = {
   lang?: Lang
@@ -31,11 +13,13 @@ export const PostListPage: FC<Props> = ({ lang, posts }) => {
   const { t } = getTranslation(lang ?? fallbackLang)
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>{t('posts.title')}</h1>
-      {posts.map((post) => (
-        <PostCard key={post.slug} {...post} />
-      ))}
+      <div className={styles.postCardList}>
+        {posts.map((post) => (
+          <PostCard key={post.slug} post={post} />
+        ))}
+      </div>
     </div>
   )
 }
