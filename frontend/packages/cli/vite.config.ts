3a0a8a18f5c26c652946b3e4242b6ce29aa5af96
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2024-12-02 18:01:58 +0900

    poc

--- frontend/packages/cli/vite.config.ts
+++ frontend/packages/cli/vite.config.ts
@@ -1,15 +1,37 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
   build: { chunkSizeWarningLimit: 1600 },
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   test: {
     globals: true,
     environment: 'node',
     coverage: {
       reporter: ['text', 'json', 'html'],
+    },
+  },
+  resolve: {
+    alias: {
+      timers: 'rollup-plugin-node-polyfills/polyfills/timers',
     },
   },
 })
