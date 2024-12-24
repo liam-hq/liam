d724421060207afee79479f807095d0bcf00ceb8
junkisai <jonki.beam@gmail.com>
2024-12-24 09:23:26 +0900

    🚸  add gradient stroke and highlight animation to RelationshipEdge component

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/RelationshipEdge/RelationshipEdge.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/RelationshipEdge/RelationshipEdge.tsx
@@ -47,6 +47,52 @@ export const RelationshipEdge: FC<Props> = ({
         }
         className={clsx(styles.edge, data?.isHighlighted && styles.hovered)}
       />
+      <defs>
+        <filter id="glowingShadow" x="-50%" y="-50%" width="200%" height="200%">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="1" result="blur" />
+          <feFlood flood-color="white" flood-opacity="0.8" result="glowColor" />
+          <feComposite
+            in="glowColor"
+            in2="blur"
+            operator="in"
+            result="softGlow"
+          />
+          <feGaussianBlur in="softGlow" stdDeviation="2" result="spread" />
+          <feMerge>
+            <feMergeNode in="spread" />
+            <feMergeNode in="softGlow" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+        <linearGradient id="myGradient" gradientTransform="rotate(45)">
+          <stop offset="0%" stop-color="white" stop-opacity="0.9" />
+          <stop offset="45%" stop-color="rgba(255, 255, 255, 0.8)" />
+          <stop offset="50%" stop-color="rgba(255, 255, 255, 1)" />
+          <stop offset="55%" stop-color="rgba(255, 255, 255, 0.8)" />
+          <stop offset="100%" stop-color="rgba(255, 255, 255, 0.3)" />
+        </linearGradient>
+      </defs>
+      {data?.isHighlighted && (
+        <rect
+          width="10"
+          height="2"
+          x="-5"
+          y="-1"
+          rx="1"
+          fill="url('#myGradient')"
+          filter="url(#brightGlow)"
+        >
+          <animateMotion
+            begin="0s"
+            dur="3s"
+            repeatCount="indefinite"
+            rotate="auto"
+            calcMode="spline"
+            keySplines="0.4 0 0.2 1"
+            path={edgePath}
+          />
+        </rect>
+      )}
     </>
   )
 }
