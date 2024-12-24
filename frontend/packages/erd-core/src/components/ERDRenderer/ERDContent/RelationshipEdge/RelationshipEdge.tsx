fdc03927e5d58053a2a40fbe84b3d41c3c4abb5e
junkisai <jonki.beam@gmail.com>
2024-12-24 09:11:19 +0900

    🚸  add gradient stroke and highlight animation to RelationshipEdge component

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/RelationshipEdge/RelationshipEdge.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/RelationshipEdge/RelationshipEdge.tsx
@@ -47,6 +47,37 @@ export const RelationshipEdge: FC<Props> = ({
         }
         className={clsx(styles.edge, data?.isHighlighted && styles.hovered)}
       />
+      <defs>
+        <linearGradient id="myGradient" gradientTransform="rotate(45)">
+          <stop offset="0%" stop-color="white" stop-opacity="0.9" />
+          <stop offset="45%" stop-color="rgba(255, 255, 255, 0.8)" />
+          <stop offset="50%" stop-color="rgba(255, 255, 255, 1)" />
+          <stop offset="55%" stop-color="rgba(255, 255, 255, 0.8)" />
+          <stop offset="100%" stop-color="rgba(255, 255, 255, 0.3)" />
+        </linearGradient>
+      </defs>
+      <path path={edgePath} stroke="url('#myGradient')" />
+
+      {data?.isHighlighted && (
+        <rect
+          width="10"
+          height="2"
+          x="-5"
+          y="-1"
+          rx="1"
+          fill="url('#myGradient')"
+        >
+          <animateMotion
+            begin="0s"
+            dur="3s"
+            repeatCount="indefinite"
+            rotate="auto"
+            calcMode="spline"
+            keySplines="0.4 0 0.2 1"
+            path={edgePath}
+          />
+        </rect>
+      )}
     </>
   )
 }
