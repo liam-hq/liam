c249f6c837302e142633fb55e1fb880a1258b706
hoshinotsuyoshi <tsuyoshi.hoshino@route06.co.jp>
2024-12-24 21:09:03 +0900

    PoC: loadPrism.ts?

---
+++ frontend/packages/db-structure/src/parser/schemarb/loadPrism.ts
@@ -0,0 +1,25 @@
+// biome-ignore lint/correctness/noNodejsModules: this is needed
+import { readFile } from 'node:fs/promises'
+// biome-ignore lint/correctness/noNodejsModules: this is needed
+import { fileURLToPath } from 'node:url'
+// biome-ignore lint/correctness/noNodejsModules: this is needed
+import { WASI } from 'node:wasi'
+import type { ParseResult } from '@ruby/prism/src/deserialize.js'
+import { parsePrism } from '@ruby/prism/src/parsePrism.js'
+
+export async function loadPrism(): Promise<(source: string) => ParseResult> {
+  const path = fileURLToPath(new URL('prism.wasm', import.meta.url))
+  const wasm = await WebAssembly.compile(await readFile(path))
+
+  const wasi = new WASI({ version: 'preview1' })
+
+  const instance = await WebAssembly.instantiate(wasm, {
+    wasi_snapshot_preview1: wasi.wasiImport,
+  })
+
+  wasi.initialize(instance)
+
+  return function parse(source: string): ParseResult {
+    return parsePrism(instance.exports, source)
+  }
+}
