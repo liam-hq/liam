de797255d6e43217f843ca46915bffceb2778299
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-26 12:50:20 +0900

    wip

---
+++ frontend/apps/erd-web/app/erd/p/[...slug]/page.tsx
@@ -0,0 +1,39 @@
+// biome-ignore lint/correctness/noNodejsModules: <explanation>
+import path from 'node:path'
+import { parse, setPrismWasmUrl } from '@liam-hq/db-structure/parser'
+// import prismWasmUrl from '@liam-hq/db-structure/parser/schemarb/prism.wasm'
+import { notFound } from 'next/navigation'
+import ERDViewer from './erdViewer'
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>
+}) {
+  const { slug } = await params
+  const joinedPath = slug.join('/')
+  if (!joinedPath) {
+    notFound()
+  }
+
+  const contentUrl = `https://${joinedPath}`
+
+  const res = await fetch(contentUrl, { cache: 'no-store' })
+  if (!res.ok) {
+    notFound()
+  }
+
+  const input = await res.text()
+
+  setPrismWasmUrl(path.resolve(process.cwd(), '.next/server/prism.wasm'))
+
+  // Currently supports Postgres only
+  const { value: dbStructure, errors } = await parse(input, 'schemarb')
+  if (errors.length > 0) {
+    for (const error of errors) {
+      console.error(error)
+    }
+  }
+
+  return <ERDViewer dbStructure={dbStructure} defaultSidebarOpen />
+}
--- frontend/apps/erd-web/next.config.ts
+++ frontend/apps/erd-web/next.config.ts
@@ -1,7 +1,20 @@
+import CopyPlugin from 'copy-webpack-plugin'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'node_modules/@liam-hq/db-structure/dist/parser/schemarb/prism.wasm',
+            to: './server/prism.wasm',
+          },
+        ],
+      }),
+    )
+    return config
+  },
 }
 
 export default nextConfig
