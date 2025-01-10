0b8e679613a63d561411a5eb65984a465433f3c3
MH4GF <h.miyagi.cnw@gmail.com>
2025-01-10 15:51:47 +0900

    🔧 Update Vite configuration to include 'react/jsx-runtime' as an external dependency

--- frontend/packages/erd-core/vite.config.ts
+++ frontend/packages/erd-core/vite.config.ts
@@ -10,11 +10,12 @@ export default defineConfig({
       fileName: 'index',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react/jsx-runtime', 'react-dom', 'react-dom/client'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
         },
       },
     },
