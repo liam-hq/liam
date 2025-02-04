69833ff2e965499a5ea6c8d19b91adb1b6330ce9
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2025-02-04 12:58:12 +0900

    🎨 Refactor Toolbar positioning with Sidebar

--- frontend/packages/erd-core/src/components/ERDRenderer/Toolbar/Toolbar.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/Toolbar/Toolbar.tsx
@@ -1,12 +1,22 @@
+import { useSidebar } from '@liam-hq/ui'
+import clsx from 'clsx'
 import type { FC } from 'react'
 import { DesktopToolbar } from './DesktopToolbar'
 import { MobileToolbar } from './MobileToolbar'
+import styles from './Toolbar.module.css'
 
 export const Toolbar: FC = () => {
+  const { state } = useSidebar()
+  const isExpanded = state === 'expanded'
+
   return (
-    <>
+    <div
+      className={clsx(styles.wrapper, {
+        [styles.wrapperWithSidebar]: isExpanded,
+      })}
+    >
       <MobileToolbar />
       <DesktopToolbar />
-    </>
+    </div>
   )
 }
