daeaf24255a64729b6a5d8520338e8e79f263282
FunamaYukina <yukina.funama@route06.co.jp>
2025-02-04 11:34:53 +0900

    E2E Tests workflow test

--- frontend/packages/cli/tests/e2e/page.test.ts
+++ frontend/packages/cli/tests/e2e/page.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test'
 
+const url = process.env.CLI_URL || 'http://localhost:5173'
+
 test('Page has title', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+  await page.goto(url)
   await expect(page).toHaveTitle('Liam ERD')
 })
--- frontend/packages/erd-core/tests/e2e/page.test.ts
+++ frontend/packages/erd-core/tests/e2e/page.test.ts
@@ -1,8 +1,10 @@
 import { expect, test } from '@playwright/test'
 
+const url =
+  process.env.ERD_WEB_URL ||
+  'http://localhost:3001/erd/p/github.com/mastodon/mastodon/blob/main/db/schema.rb'
+
 test('Page has title', async ({ page }) => {
-  await page.goto(
-    'http://localhost:3001/erd/p/github.com/mastodon/mastodon/blob/main/db/schema.rb',
-  )
+  await page.goto(url)
   await expect(page).toHaveTitle(/Liam ERD/)
 })
