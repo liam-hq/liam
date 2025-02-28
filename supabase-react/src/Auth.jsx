36865196678c4b930cfafe6904fe72748b242af3
hoshinotsuyoshi <tsuyoshi.hoshino@route06.co.jp>
2025-02-28 14:08:31 +0900

    make it GitHub Login

--- supabase-react/src/Auth.jsx
+++ supabase-react/src/Auth.jsx
@@ -9,12 +9,12 @@ export default function Auth() {
     event.preventDefault()
 
     setLoading(true)
-    const { error } = await supabase.auth.signInWithOtp({ email })
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github'
+    })
 
     if (error) {
       alert(error.error_description || error.message)
-    } else {
-      alert('Check your email for the login link!')
     }
     setLoading(false)
   }
@@ -23,21 +23,13 @@ export default function Auth() {
     <div className="row flex flex-center">
       <div className="col-6 form-widget">
         <h1 className="header">Supabase + React</h1>
-        <p className="description">Sign in via magic link with your email below</p>
+        <p className="description">GitHub Login Sample Flow.</p>
         <form className="form-widget" onSubmit={handleLogin}>
           <div>
-            <input
-              className="inputField"
-              type="email"
-              placeholder="Your email"
-              value={email}
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-            />
           </div>
           <div>
             <button className={'button block'} disabled={loading}>
-              {loading ? <span>Loading</span> : <span>Send magic link</span>}
+              {loading ? <span>Loading</span> : <span>GitHub Login Button</span>}
             </button>
           </div>
         </form>
