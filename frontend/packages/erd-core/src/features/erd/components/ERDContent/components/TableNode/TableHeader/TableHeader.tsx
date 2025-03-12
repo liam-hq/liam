9a24d731372e1f64ffb6b6082e71428b0cad0ea7
Prakhar Dewan <33227141+prakha@users.noreply.github.com>
2025-03-13 02:04:26 +0530

    removed tooltip modules from table header file

--- frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableHeader/TableHeader.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableHeader/TableHeader.tsx
@@ -1,41 +1,36 @@
-import type { TableNodeData } from "@/features/erd/types";
-import { useUserEditingStore } from "@/stores";
+import type { TableNodeData } from '@/features/erd/types'
+import { useUserEditingStore } from '@/stores'
 import {
   Table2,
-  TooltipContent,
-  TooltipPortal,
-  TooltipProvider,
-  TooltipRoot,
-  TooltipTrigger,
-} from "@liam-hq/ui";
-import { Handle, Position } from "@xyflow/react";
-import clsx from "clsx";
-import type { FC } from "react";
-import styles from "./TableHeader.module.css";
+} from '@liam-hq/ui'
+import { Handle, Position } from '@xyflow/react'
+import clsx from 'clsx'
+import type { FC } from 'react'
+import styles from './TableHeader.module.css'
 
 type Props = {
-  data: TableNodeData;
-};
+  data: TableNodeData
+}
 
 export const TableHeader: FC<Props> = ({ data }) => {
-  const name = data.table.name;
-  const { showMode } = useUserEditingStore();
+  const name = data.table.name
+  const { showMode } = useUserEditingStore()
 
-  const isTarget = data.targetColumnCardinalities !== undefined;
-  const isSource = data.sourceColumnName !== undefined;
+  const isTarget = data.targetColumnCardinalities !== undefined
+  const isSource = data.sourceColumnName !== undefined
 
   return (
     <div
       className={clsx(
         styles.wrapper,
-        showMode === "TABLE_NAME" && styles.wrapperTableNameMode
+        showMode === 'TABLE_NAME' && styles.wrapperTableNameMode,
       )}
     >
       <Table2 width={16} className={styles.tableIcon} />
 
       <span className={styles.name}>{name}</span>
 
-      {showMode === "TABLE_NAME" && (
+      {showMode === 'TABLE_NAME' && (
         <>
           {isTarget && (
             <Handle
@@ -56,5 +51,5 @@ export const TableHeader: FC<Props> = ({ data }) => {
         </>
       )}
     </div>
-  );
-};
+  )
+}
--- frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableNode.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableNode.tsx
@@ -1,11 +1,11 @@
-import type { TableNodeType } from "@/features/erd/types";
-import { useUserEditingStore } from "@/stores";
-import type { NodeProps } from "@xyflow/react";
-import clsx from "clsx";
-import type { FC } from "react";
-import { TableColumnList } from "./TableColumnList";
-import { TableHeader } from "./TableHeader";
-import styles from "./TableNode.module.css";
+import type { TableNodeType } from '@/features/erd/types'
+import { useUserEditingStore } from '@/stores'
+import type { NodeProps } from '@xyflow/react'
+import clsx from 'clsx'
+import type { FC } from 'react'
+import { TableColumnList } from './TableColumnList'
+import { TableHeader } from './TableHeader'
+import styles from './TableNode.module.css'
 
 import {
   TooltipContent,
@@ -13,12 +13,12 @@ import {
   TooltipProvider,
   TooltipRoot,
   TooltipTrigger,
-} from "@liam-hq/ui";
+} from '@liam-hq/ui'
 
-type Props = NodeProps<TableNodeType>;
+type Props = NodeProps<TableNodeType>
 
 export const TableNode: FC<Props> = ({ data }) => {
-  const { showMode } = useUserEditingStore();
+  const { showMode } = useUserEditingStore()
 
   return (
     <TooltipProvider>
@@ -28,26 +28,26 @@ export const TableNode: FC<Props> = ({ data }) => {
             className={clsx(
               styles.wrapper,
               data.isHighlighted && styles.wrapperHighlighted,
-              data.isActiveHighlighted && styles.wrapperActive
+              data.isActiveHighlighted && styles.wrapperActive,
             )}
             data-erd={
               (data.isHighlighted || data.isActiveHighlighted) &&
-              "table-node-highlighted"
+              'table-node-highlighted'
             }
           >
             <TableHeader data={data} />
-            {showMode === "ALL_FIELDS" && <TableColumnList data={data} />}
-            {showMode === "KEY_ONLY" && (
+            {showMode === 'ALL_FIELDS' && <TableColumnList data={data} />}
+            {showMode === 'KEY_ONLY' && (
               <TableColumnList data={data} filter="KEY_ONLY" />
             )}
           </div>
         </TooltipTrigger>
         <TooltipPortal>
-          <TooltipContent side={"top"} sideOffset={4}>
+          <TooltipContent side={'top'} sideOffset={4}>
             {data?.table?.name}
           </TooltipContent>
         </TooltipPortal>
       </TooltipRoot>
     </TooltipProvider>
-  );
-};
+  )
+}
