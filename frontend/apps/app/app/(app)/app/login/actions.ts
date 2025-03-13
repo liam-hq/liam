fe577448eb03b161627ecb1135fdc5b24f1e5880
MH4GF <h.miyagi.cnw@gmail.com>
2025-03-13 19:07:27 +0900

    debug

--- frontend/apps/app/app/(app)/app/login/actions.ts
+++ frontend/apps/app/app/(app)/app/login/actions.ts
@@ -10,10 +10,8 @@ function getAuthCallbackUrl({
   next = '/app',
   provider,
 }: { next?: string; provider: OAuthProvider }): string {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ??
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-    'http://localhost:3001/'
+  let url = process.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3001/'
+  url = url.startsWith('http') ? url : `https://${url}`
   url = url.endsWith('/') ? url : `${url}/`
   return `${url}app/auth/callback/${provider}?next=${encodeURIComponent(next)}`
 }
@@ -22,12 +20,15 @@ export async function login() {
   const supabase = await createClient()
 
   const redirectTo = getAuthCallbackUrl({ provider: 'github' })
+  console.info({ redirectTo, url: process.env?.NEXT_PUBLIC_VERCEL_URL })
 
   const provider = 'github'
   const { error, data } = await supabase.auth.signInWithOAuth({
     provider,
     options: { redirectTo },
   })
+
+  console.info({ error, data })
 
   if (error) {
     redirect('/error')
