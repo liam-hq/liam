73576166410f7e4f186620607eb47dd72161b2e4
hoshinotsuyoshi <tsuyoshi.hoshino@route06.co.jp>
2025-03-13 19:20:53 +0900

    $ pnpm --filter @liam-hq/db run migrate:dev x

---
+++ frontend/packages/db/supabase/migrations/20250313101938_x.sql
@@ -0,0 +1,43 @@
+revoke delete on table "public"."OverallReview" from "anon";
+
+revoke insert on table "public"."OverallReview" from "anon";
+
+revoke references on table "public"."OverallReview" from "anon";
+
+revoke select on table "public"."OverallReview" from "anon";
+
+revoke trigger on table "public"."OverallReview" from "anon";
+
+revoke truncate on table "public"."OverallReview" from "anon";
+
+revoke update on table "public"."OverallReview" from "anon";
+
+revoke delete on table "public"."OverallReview" from "authenticated";
+
+revoke insert on table "public"."OverallReview" from "authenticated";
+
+revoke references on table "public"."OverallReview" from "authenticated";
+
+revoke select on table "public"."OverallReview" from "authenticated";
+
+revoke trigger on table "public"."OverallReview" from "authenticated";
+
+revoke truncate on table "public"."OverallReview" from "authenticated";
+
+revoke update on table "public"."OverallReview" from "authenticated";
+
+revoke delete on table "public"."OverallReview" from "service_role";
+
+revoke insert on table "public"."OverallReview" from "service_role";
+
+revoke references on table "public"."OverallReview" from "service_role";
+
+revoke select on table "public"."OverallReview" from "service_role";
+
+revoke trigger on table "public"."OverallReview" from "service_role";
+
+revoke truncate on table "public"."OverallReview" from "service_role";
+
+revoke update on table "public"."OverallReview" from "service_role";
+
+
