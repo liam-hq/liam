db7136caf528e7315ea2329d9e0e5d5449b52896
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2025-03-14 15:41:04 +0900

    🔍 Add logging for environment variables in POST route of TestTable API

--- frontend/apps/app/app/api/testtable/route.ts
+++ frontend/apps/app/app/api/testtable/route.ts
@@ -12,6 +12,32 @@ export const POST = async (request: Request): Promise<NextResponse> => {
       )
     }
 
+    console.info('POSTGRES_URL:', process.env.POSTGRES_URL)
+    console.info('POSTGRES_PRISMA_URL:', process.env.POSTGRES_PRISMA_URL)
+    console.info(
+      'POSTGRES_URL_NON_POOLING:',
+      process.env.POSTGRES_URL_NON_POOLING,
+    )
+    console.info('POSTGRES_USER:', process.env.POSTGRES_USER)
+    console.info('POSTGRES_HOST:', process.env.POSTGRES_HOST)
+    console.info('POSTGRES_PASSWORD:', process.env.POSTGRES_PASSWORD)
+    console.info('POSTGRES_DATABASE:', process.env.POSTGRES_DATABASE)
+    console.info(
+      'SUPABASE_SERVICE_ROLE_KEY:',
+      process.env.SUPABASE_SERVICE_ROLE_KEY,
+    )
+    console.info('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY)
+    console.info('SUPABASE_URL:', process.env.SUPABASE_URL)
+    console.info('SUPABASE_JWT_SECRET:', process.env.SUPABASE_JWT_SECRET)
+    console.info(
+      'NEXT_PUBLIC_SUPABASE_ANON_KEY:',
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    )
+    console.info(
+      'NEXT_PUBLIC_SUPABASE_URL:',
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+    )
+
     const pullRequest = await prisma.pullRequest.findFirst({
       where: {
         id: 1,
