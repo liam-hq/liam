4358951f7e6c2337d5c08a0c40d6b38cafaaba4f
FunamaYukina <yukina.funama@route06.co.jp>
2025-05-12 21:10:59 +0900

    fix lint error

--- frontend/packages/jobs/src/index.ts
+++ frontend/packages/jobs/src/index.ts
@@ -3,13 +3,23 @@ import '@mastra/core'
 // Then import our Mastra instance
 import './mastra'
 
+// Task exports
 export {
   savePullRequestTask,
   generateReviewTask,
   saveReviewTask,
   postCommentTask,
 } from './mastra/tasks/review'
+
+// Function exports
+export { processGenerateReview } from './mastra/tasks/review/generateReview'
+export { postComment } from './mastra/tasks/review/postComment'
+export { processSavePullRequest } from './mastra/tasks/review/savePullRequest'
+export { processSaveReview } from './mastra/tasks/review/saveReview'
+
+// Trigger exports
 export {
   helloWorld,
   generateKnowledgeFromFeedbackTask,
+  createKnowledgeSuggestionTask,
 } from './mastra/trigger/jobs'
