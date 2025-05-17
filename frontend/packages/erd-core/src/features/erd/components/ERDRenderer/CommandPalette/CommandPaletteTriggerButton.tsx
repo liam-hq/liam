f58c6e4cadb5c299211e99f3d728e272c561dca9
Tomoya Kashifuku <tnyo43@gmail.com>
2025-05-17 17:18:03 +0900

    make button prettier

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPaletteTriggerButton.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPaletteTriggerButton.tsx
@@ -1,10 +1,22 @@
 import { updatePaletteOpen } from '@/stores'
+import { Search } from '@liam-hq/ui'
 import type { FC } from 'react'
+import styles from './CommandPalette.module.css'
+import { CmdIcon, KIcon } from './Icons'
 
 export const CommandPaletteTriggerButton: FC = () => {
   return (
-    <button type="button" onClick={() => updatePaletteOpen(true)}>
-      search
+    <button
+      type="button"
+      className={styles.triggerButtonContainer}
+      onClick={() => updatePaletteOpen(true)}
+    >
+      <Search />
+      Search
+      <span className={styles.rightSide}>
+        <CmdIcon />
+        <KIcon />
+      </span>
     </button>
   )
 }
