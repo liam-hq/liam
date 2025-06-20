ad158a2d953c5998717e34cf6524ef72da843854
khiroshi-win <hiroshi.kaji@route06.co.jp>
2025-06-20 15:17:41 +0900

    revert: playwright.config.ts

--- frontend/internal-packages/e2e/playwright.config.ts
+++ frontend/internal-packages/e2e/playwright.config.ts
@@ -7,14 +7,12 @@ export default defineConfig({
   forbidOnly: !!process.env['CI'],
   retries: process.env['CI'] ? 5 : 0,
   workers: process.env['CI'] ? 1 : '50%',
-  timeout: 20 * 1000,
+  timeout: 10 * 1000,
   reporter: 'html',
   use: {
     baseURL: process.env['URL'] || 'http://localhost:5173',
     trace: 'on-first-retry',
     storageState: 'storageState.json',
-    actionTimeout: 8000,
-    navigationTimeout: 8000,
   },
 
   projects: [
