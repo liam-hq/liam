2531b3c595998152485ec1020328a68e6818d44d
Tomoya Kashifuku <tnyo43@gmail.com>
2025-07-31 09:04:16 +0900

    ✨ separate header and command palette toasts

--- frontend/packages/ui/src/components/Toast/useToast.tsx
+++ frontend/packages/ui/src/components/Toast/useToast.tsx
@@ -1,4 +1,7 @@
 import { useContext } from 'react'
 import { ToastContext } from './Toast'
 
-export const useToast = () => useContext(ToastContext)
+export const useToast = () => useContext(ToastContext).headerToast
+
+export const useCommandPaletteToast = () =>
+  useContext(ToastContext).commandPaletteToast
