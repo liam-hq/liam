bfaf3cb7a79149203b80e1a3265d119c70995ebc
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-23 15:17:54 +0900

    ✨ create CommandPreview component with Copy Link gif image

---
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPalettePreview/CommandPreview.tsx
@@ -0,0 +1,19 @@
+import type { FC } from 'react'
+import CopyLinkPreview from './asserts/copy-link-preview.gif'
+import styles from './CommandPalettePreview.module.css'
+
+export const CommandPreview: FC = () => {
+  return (
+    <img
+      className={styles.previewImage}
+      src={
+        typeof CopyLinkPreview === 'string'
+          ? CopyLinkPreview
+          : typeof CopyLinkPreview.src === 'string'
+            ? CopyLinkPreview.src
+            : undefined
+      }
+      alt=""
+    />
+  )
+}
