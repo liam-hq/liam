4f0be4d5d68187d6c2fc9e03e97badbab2156d1f
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-31 13:40:29 +0900

    fix lint

--- frontend/packages/erd-core/src/images.d.ts
+++ frontend/packages/erd-core/src/images.d.ts
@@ -1,4 +1,9 @@
 declare module '*.png' {
-  const value: string
+  const value: string | import('next/image').StaticImageData
   export default value
+}
+
+declare module '*.mp4' {
+  const content: string
+  export default content
 }
--- frontend/packages/erd-core/src/types/images.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.mp4' {
-  const content: string
-  export default content
-}
-declare module '*.png' {
-  const content: string | { src: string }
-  export default content
-}
