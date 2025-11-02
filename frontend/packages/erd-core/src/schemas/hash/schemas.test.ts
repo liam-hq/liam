92b8eb0182b5e29c734d2f88e9127fece4e0e159
Tomoya Kashifuku <tnyo43@gmail.com>
2025-11-02 12:58:32 +0900

    ✨ extend hash schema

--- frontend/packages/erd-core/src/schemas/hash/schemas.test.ts
+++ frontend/packages/erd-core/src/schemas/hash/schemas.test.ts
@@ -1,15 +1,22 @@
 import { parse } from 'valibot'
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { hashSchema } from './schemas'
 
-it('should pass valid texts', () => {
-  expect(parse(hashSchema, 'users__columns__id')).toBe('users__columns__id')
-  expect(parse(hashSchema, 'user_posts__columns__post_id')).toBe(
-    'user_posts__columns__post_id',
-  )
-  expect(parse(hashSchema, 'posts__indexes__index_post_on_user_id')).toBe(
-    'posts__indexes__index_post_on_user_id',
-  )
+describe('should pass valid texts', () => {
+  it('category level hash', () => {
+    expect(parse(hashSchema, 'users__columns')).toBe('users__columns')
+    expect(parse(hashSchema, 'posts__indexes')).toBe('posts__indexes')
+  })
+
+  it('item level hash', () => {
+    expect(parse(hashSchema, 'users__columns__id')).toBe('users__columns__id')
+    expect(parse(hashSchema, 'user_posts__columns__post_id')).toBe(
+      'user_posts__columns__post_id',
+    )
+    expect(parse(hashSchema, 'posts__indexes__index_post_on_user_id')).toBe(
+      'posts__indexes__index_post_on_user_id',
+    )
+  })
 })
 
 it('should throw error with invalid texts', () => {
--- frontend/packages/erd-core/src/schemas/hash/schemas.ts
+++ frontend/packages/erd-core/src/schemas/hash/schemas.ts
@@ -3,11 +3,21 @@ import { custom } from 'valibot'
 type HashType = 'columns' | 'indexes'
 const hashTypes = ['columns', 'indexes'] as const satisfies HashType[]
 
-export const hashSchema = custom<`${string}__${HashType}__${string}`>(
-  (input): input is `${string}__${HashType}__${string}` => {
+type HashSchemaType =
+  | `${string}__${HashType}` // category level hash
+  | `${string}__${HashType}__${string}` // item level hash
+
+export const hashSchema = custom<HashSchemaType>(
+  (input): input is HashSchemaType => {
     if (typeof input !== 'string') return false
 
     return hashTypes.some((hashType) => {
+      // category level hash
+      if (input.endsWith(`__${hashType}`)) {
+        return true
+      }
+
+      // item level hash
       const parts = input.split(`__${hashType}__`)
       return parts.length === 2 && parts[0] && parts[1]
     })
