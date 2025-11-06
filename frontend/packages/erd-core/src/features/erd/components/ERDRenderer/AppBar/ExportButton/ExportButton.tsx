9477371ad817ac349d092e1213a09e570563c255
Tomoya Kashifuku <tnyo43@gmail.com>
2025-11-07 08:58:21 +0900

    ✨ add dropdown feature

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/AppBar/ExportButton/ExportButton.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/AppBar/ExportButton/ExportButton.tsx
@@ -1,11 +1,44 @@
-import { Download } from '@liam-hq/ui'
+import {
+  Copy,
+  Download,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuRoot,
+  DropdownMenuTrigger,
+} from '@liam-hq/ui'
 import type { FC } from 'react'
 import styles from './ExportButton.module.css'
 
 export const ExportButton: FC = () => {
+  const handleCopyPostgreSQL = async () => {}
+  const handleCopyYaml = async () => {}
+
   return (
-    <button type="button" className={styles.iconWrapper}>
-      <Download className={styles.icon} />
-    </button>
+    <DropdownMenuRoot>
+      <DropdownMenuTrigger asChild>
+        <button type="button" className={styles.iconWrapper}>
+          <Download className={styles.icon} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent align="end" sideOffset={4}>
+          <DropdownMenuItem
+            size="sm"
+            leftIcon={<Copy size={16} />}
+            onSelect={handleCopyPostgreSQL}
+          >
+            Copy PostgreSQL
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            size="sm"
+            leftIcon={<Copy size={16} />}
+            onSelect={handleCopyYaml}
+          >
+            Copy YAML
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenuRoot>
   )
 }
